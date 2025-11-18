1c93660395ee9606731603a5f88e539b20147012
Maxence Badin Léger <maxencelger@gmail.com>
2025-11-18 15:13:48 +0100

    add styles base + components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Azul: Infinity Stones</h1>
+      <p>Game setup in progress...</p>
+    </div>
+  );
+}
+
+export default App;
