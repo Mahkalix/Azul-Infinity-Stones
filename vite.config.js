1568e14be5fc59e8cb7c167130da2ff2e1aa926e
Matheo <mat@gmail>
2026-01-27 10:03:10 +0100

    fix: correction des avertissements

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
   server: {
     port: 3000,
-    open: true
   },
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  }
-})
+});
